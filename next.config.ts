d00e16b84faaa24fec99b18a24767a87eeeee689
ExquisiteCore <xiaolongqq1y@163.com>
2025-04-03 14:20:58 +0800

    chore: 更新项目配置以支持Tauri集成
    - 在package.json中添加tauri脚本
    - 更新tauri.conf.json中的应用标识符
    - 配置next.config.ts以支持Tauri开发环境
    - 调整tsconfig.json中的编译器选项
    - 更新Cargo.toml中的依赖配置

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const internalHost = process.env.TAURI_DEV_HOST || "localhost";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // 确保 Next.js 使用 SSG 而不是 SSR
+  output: "export",
+  // 确保 Next.js 不会优化您的图像。
+  images: {
+    unoptimized: true,
+  },
+  // 配置 assetPrefix，否则服务器无法正确解析您的资产。
+  assetPrefix: isProd ? undefined : `http://${internalHost}:3000`,
 };
 
 export default nextConfig;
