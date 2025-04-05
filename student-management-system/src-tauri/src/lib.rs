82b5fa691f864e1070ee3a8839fa6b17f38506ab
ExquisiteCore <xiaolongqq1y@163.com>
2025-04-05 14:11:20 +0800

    refactor: 移除未使用的依赖项和配置，简化代码结构
    移除未使用的依赖项和配置文件，包括数据库配置和相关的Rust库。简化了`main.rs`和`lib.rs`中的代码结构，并更新了`Cargo.toml`以移除不必要的依赖项。这些更改提高了代码的可维护性和清晰度。

--- student-management-system/src-tauri/src/lib.rs
+++ student-management-system/src-tauri/src/lib.rs
@@ -1,19 +1,16 @@
-pub mod config;
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
-        .setup(|app| {
-            if cfg!(debug_assertions) {
-                app.handle().plugin(
-                    tauri_plugin_log::Builder::default()
-                        .level(log::LevelFilter::Info)
-                        .build(),
-                )?;
-                log::info!("应用程序初始化完成");
-            }
-            Ok(())
-        })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .setup(|app| {
+      if cfg!(debug_assertions) {
+        app.handle().plugin(
+          tauri_plugin_log::Builder::default()
+            .level(log::LevelFilter::Info)
+            .build(),
+        )?;
+      }
+      Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
--- student-management-system/src-tauri/src/main.rs
+++ student-management-system/src-tauri/src/main.rs
@@ -1,20 +1,6 @@
-use std::path::Path;
-
-use app_lib::config;
-
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#[cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 fn main() {
-    app_lib::run();
-    // 加载配置文件
-    let config_path = Path::new("config.toml");
-    let config = if config_path.exists() {
-        log::info!("从配置文件加载配置: {:?}", config_path);
-        config::Config::from_file(config_path).unwrap()
-    } else {
-        log::info!("使用默认配置");
-        config::Config::default()
-    };
-    // 初始化全局配置
-    config::init_config(config.clone());
+  app_lib::run();
 }
